3bd1c2416edb751787b53eef7c6d4755e3a7a709
seinaas <assadian.seina@gmail.com>
2020-04-14 18:05:33 -0400

    Updated portfolio for assignment 2

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.run()
